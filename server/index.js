884c7db8871c25148a2708511eb0853d18cd0626
sonnv <hailos1000@gmail.com>
2020-06-23 23:15:05 +0700

    update for production

--- server/index.js
+++ server/index.js
@@ -6,8 +6,8 @@ require('./config/mongoose');
 // module.parent check is required to support mocha watch
 // src: https://github.com/mochajs/mocha/issues/1912
 if (!module.parent) {
-  app.listen(config.port, () => {
-    console.info(`server started on port ${config.port} (${config.env})`);
+  app.listen(process.env.PORT || config.port, () => {
+    console.info(`server started on port ${process.env.PORT || config.port} (${config.env})`);
   });
 }
 
