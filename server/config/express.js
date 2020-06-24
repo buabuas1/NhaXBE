00b6e8cb3319d4e162df77395827c6c389d3a1e4
sonnv <hailos1000@gmail.com>
2020-06-24 23:03:30 +0700

    update config

--- server/config/express.js
+++ server/config/express.js
@@ -35,14 +35,14 @@ app.use(/^((?!(api)).)*/, (req, res) => {
   res.sendFile(path.join(__dirname, distDir + '/index.html'));
 });
 
-// console.log('distDir: ', distDir);
-// console.log('__dirname: ', __dirname);
-// console.log('full: ', path.join(__dirname, distDir + '/index.html'));
+console.log('distDir: ', distDir);
+console.log('__dirname: ', __dirname);
+console.log('full: ', path.join(__dirname, distDir + '/index.html'));
  //React server
-// app.use(express.static(path.join(__dirname, '../../node_modules/material-dashboard-react/dist')))
-// app.use(/^((?!(api)).)*/, (req, res) => {
-// res.sendFile(path.join(__dirname, '../../dist/index.html'));
-// });
+app.use(express.static(path.join(__dirname, '../../node_modules/material-dashboard-react/dist')))
+app.use(/^((?!(api)).)*/, (req, res) => {
+res.sendFile(path.join(__dirname, '../../dist/index.html'));
+});
 
 
 app.use(bodyParser.json());
